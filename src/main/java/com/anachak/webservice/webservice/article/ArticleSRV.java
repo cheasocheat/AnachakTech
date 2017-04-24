43eceaca36ad57e5c23c92dd398404eff0a4aa09
Chea Socheat <cheacheat.kh@gmail.com>
2017-04-24 17:29:12 +0700

    Recorect servlet container

---
+++ src/main/java/com/anachak/webservice/webservice/article/ArticleSRV.java
@@ -0,0 +1,41 @@
+package com.anachak.webservice.webservice.article;
+
+import com.anachak.webservice.model.Article;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cheasocheat on 4/6/17.
+ */
+@Path("article")
+public class ArticleSrv {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getArticle() {
+        return "HELLO ARTICLE";
+    }
+
+    @GET
+    @Path("/list_articles")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<Article> listAllArticle(){
+        List<Article> lstArticles = new ArrayList<>();
+        Article article = null;
+        for (int i = 1; i <= 10; i++) {
+            article = new Article();
+            article.setId(i);
+            article.setSummary("Article " + i);
+            article.setDescription("Desc " + i);
+            lstArticles.add(article);
+        }
+        return lstArticles;
+    }
+}
